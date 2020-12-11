4522764164abde0884f444db9c5c9f9e1964a396
zhiyi98 <742801893@qq.com>
2020-12-11 20:25:14 +0800

    使用mock

---
+++ src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png" />
+    <router-view />
+    <button @click="getClick">getClick</button>
+    <button @click="postClick">postClick</button>
+
+    <div v-for="(item, index) in getData" :key="index">
+      <div v-if="index < 4">
+        <div>
+          <h2>{{ item.id }}</h2>
+          <h2>{{ item.title }}</h2>
+          <h2>{{ item.name }}</h2>
+          <h2>{{ item.emial }}</h2>
+          <h2>{{ item.paraprath }}</h2>
+          <h2>{{ item.url }}</h2>
+          <h2>{{ item.word }}</h2>
+          <a href="#">
+            <img :src="item.image" alt="">
+          </a>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import mock from "../mock/mock.js";
+import axios from "axios";
+
+export default {
+  name: "App",
+  data() {
+    return {
+      getData: [],
+      postData: [],
+    };
+  },
+  methods: {
+    getClick() {
+      axios.get("/get").then((res) => {
+        console.log(res);
+        this.getData = res.data;
+      });
+    },
+    postClick() {
+      axios.post("/post").then((res) => {
+        console.log(res);
+      });
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
